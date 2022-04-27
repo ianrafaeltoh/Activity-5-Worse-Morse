717683826b8902344265d02fedbac2a873c9fa0a
ianrafaeltoh <103250409+ianrafaeltoh@users.noreply.github.com>
2022-04-27 13:01:28 +0800

    Add files via upload

---
+++ WorseMorse.dart
@@ -0,0 +1,59 @@
+import 'dart:core';
+import 'dart:io';
+
+void main() {
+  var result = [];
+  final morseMap = <String, String>{
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z"
+  };
+
+  void Translator(String code, String decodedValue) {
+    if (code.length == 0 || code.isEmpty) {
+      result.add(decodedValue);
+    }
+    for (int i = 1; i <= code.length && i <= 5; i++) {
+      String temp = code.substring(0, i);
+      if (code.contains(temp)) {
+        var temp2 = morseMap[temp];
+        Translator(code.substring(i), decodedValue + temp2!);
+      }
+    }
+  }
+
+  void runMe() {
+    print("Input Morse Code: ");
+    String? usrin = stdin.readLineSync();
+    Translator(usrin!, "");
+    print("Input Accepted: $usrin");
+    for (String s in result) {
+      print(s);
+    }
+  }
+
+  runMe();
+}
