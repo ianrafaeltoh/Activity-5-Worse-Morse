6554c1cb3ace85f548c6f0222538a20659d3ab29
ianrafaeltoh <103250409+ianrafaeltoh@users.noreply.github.com>
2022-04-27 15:44:33 +0800

    Update WorseMorse.dart

--- WorseMorse.dart
+++ WorseMorse.dart
@@ -1,7 +1,7 @@
 import 'dart:core';
 import 'dart:io';
 
-final morseMap = <String, String>{
+var morseMap = <String, String>{
   ".-": "a",
   "-...": "b",
   "-.-.": "c",
@@ -32,7 +32,7 @@ final morseMap = <String, String>{
 var result = [];
 void main() {
   print("Input Morse Code: ");
-  String? usrin = stdin.readLineSync();
+  var usrin = stdin.readLineSync();
   Translator(usrin!, "");
   print("Input Accepted: $usrin");
   for (String s in result) {
@@ -41,12 +41,12 @@ void main() {
 }
 
 void Translator(String code, String decodedValue) {
-  if (code.length == 0 || code.isEmpty) {
+  if (code.length == 0) {
     result.add(decodedValue);
   }
   for (int i = 1; i <= code.length && i <= 5; i++) {
     String temp = code.substring(0, i);
-    if (code.contains(temp)) {
+    if (morseMap.containsKey(temp)) {
       var temp2 = morseMap[temp];
       Translator(code.substring(i), decodedValue + temp2!);
     }
